939d6570e7ade303a332d75b838f59017bd4a7bb
atom0520 <atom0520@hotmail.com>
2019-06-17 23:33:25 -0600

    Updated on 2019-06-17.

--- BestCSStudy.API/Data/Seed.cs
+++ BestCSStudy.API/Data/Seed.cs
@@ -13,34 +13,34 @@ namespace BestCSStudy.API.Data
             _context = context;
         }
 
-        public void SeedUsers()
-        {
-            if(!_context.Users.Any()){
-                var userData = System.IO.File.ReadAllText("Data/UserSeedData.json");
-                var users = JsonConvert.DeserializeObject<List<User>>(userData);
+        // public void SeedUsers()
+        // {
+        //     if(!_context.Users.Any()){
+        //         var userData = System.IO.File.ReadAllText("Data/UserSeedData.json");
+        //         var users = JsonConvert.DeserializeObject<List<User>>(userData);
                 
-                foreach (var user in users){
-                    byte[] passwordHash, passwordSalt;
-                    CreatePasswordHash("password", out passwordHash, out passwordSalt);
+        //         foreach (var user in users){
+        //             byte[] passwordHash, passwordSalt;
+        //             CreatePasswordHash("password", out passwordHash, out passwordSalt);
                     
-                    user.PasswordHash = passwordHash;
-                    user.PasswordSalt = passwordSalt;
-                    user.Username = user.Username.ToLower();
+        //             user.PasswordHash = passwordHash;
+        //             user.PasswordSalt = passwordSalt;
+        //             user.Username = user.Username.ToLower();
 
-                    _context.Users.Add(user);
-                }
+        //             _context.Users.Add(user);
+        //         }
 
-                _context.SaveChanges();
-            }    
-        }
+        //         _context.SaveChanges();
+        //     }    
+        // }
 
-        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
-        {
-            using (var hmac = new System.Security.Cryptography.HMACSHA512())
-            {
-                passwordSalt = hmac.Key;
-                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-            }
-        }
+        // private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        // {
+        //     using (var hmac = new System.Security.Cryptography.HMACSHA512())
+        //     {
+        //         passwordSalt = hmac.Key;
+        //         passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        //     }
+        // }
     }
 }
